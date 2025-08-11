8d2d7c7dc4c94bb8a00a81cf29f0aac4fec28481
DESKTOP-UR1OUJN\USER <jovan.hariono@pancabudi.com>
2025-08-11 14:10:26 +0700

    first commit

---
+++ belajar-spring-monitoring/belajar-spring-monitoring/src/test/java/programmer_zaman_now/monitoring/BelajarSpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package programmer_zaman_now.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
