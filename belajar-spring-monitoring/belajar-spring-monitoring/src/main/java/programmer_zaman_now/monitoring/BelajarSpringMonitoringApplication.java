7bc780c2fe6bc0bb5b5af2b81c832e2894f9a53e
DESKTOP-UR1OUJN\USER <jovan.hariono@pancabudi.com>
2025-08-12 10:12:27 +0700

    255

--- belajar-spring-monitoring/belajar-spring-monitoring/src/main/java/programmer_zaman_now/monitoring/BelajarSpringMonitoringApplication.java
+++ belajar-spring-monitoring/belajar-spring-monitoring/src/main/java/programmer_zaman_now/monitoring/BelajarSpringMonitoringApplication.java
@@ -2,8 +2,10 @@ package programmer_zaman_now.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BelajarSpringMonitoringApplication {
 
 	public static void main(String[] args) {
